66d78f016b0b59bb27e3a5fa5081f072db3570bd
Tade77 <adekunletaofeeq@mail.com>
2023-11-24 21:47:33 +0100

    WIP: question box

--- src/QuizComponent/Quiz.jsx
+++ src/QuizComponent/Quiz.jsx
@@ -4,10 +4,43 @@ import "./style.scss";
 const Quiz = ({ questions }) => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const { question, choices, correctAnswer } = questions[currentQuestion];
+  const [answerIdx, setAnswerIdx] = useState(null);
+  const [answer, setAnswer] = useState(false);
+
+  const onClickAnswer = (answer, index) => {
+    setAnswerIdx(index);
+    if (answer === correctAnswer) {
+      setAnswer(true);
+    } else {
+      setAnswer(false);
+    }
+  };
+  const onClickNext = () => {};
   return (
     <div className="quiz-container">
       <span className="active-question-no">{currentQuestion + 1}</span>
       <span className="totalQuestion">/{questions.length}</span>
+      <h1>{question}</h1>
+      <ul className="choices-list">
+        {choices.map((options, index) => (
+          <li
+            key={index}
+            onClick={() => onClickAnswer(answer, index)}
+            className={answerIdx === index ? "selected-answer" : null}
+          >
+            {options}
+          </li>
+        ))}
+      </ul>
+      <div className="footer">
+        <button
+          className="btn"
+          disabled={answerIdx === null}
+          onClick={onClickNext}
+        >
+          {currentQuestion === questions.length - 1 ? "Finished" : "Next"}
+        </button>
+      </div>
     </div>
   );
 };
